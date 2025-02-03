5b36e9d7d0feff9eb3123fb1a3e6d1c4bef96961
M Vishnu vardhan <vishnuvardhan7823@gmail.com>
2025-02-03 21:44:08 +0530

    secondlargest problem

--- arrayproblems/secondlargest.java
+++ arrayproblems/secondlargest.java
@@ -12,7 +12,19 @@ class secondlargest{
 
     static int secondlarge(int[] arr){
 
-        Arrays.sort(arr);
+        // Arrays.sort(arr);
+       int temp = 0;
+        for(int i=0;i<arr.length;i++){
+
+            for(int j=i+1;j<arr.length;j++){
+
+                if(arr[i]>arr[j]){
+                     temp = arr[i];
+                     arr[i] = arr[j];
+                     arr[j] = temp;
+                }
+            }
+        }
 
         for(int i=arr.length-2;i>=0;i--){
             if(arr[i]!=arr[arr.length-1]){
